ecd7e4a549358428431fb58ac68d03da9ac62585
fabianhinz <hinzfabian.fh@gmail.com>
2019-10-26 14:08:38 +0200

    init firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,41 @@
 import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
+import 'firebase/performance'
 
 import firebase from 'firebase/app'
 
-const firebaseConfig = {}
+const firebaseConfig = {
+    apiKey: 'AIzaSyD2JrQImlrqCWv749L52Z8FM6WQd0B88fs',
+    authDomain: 'iib-swe-ws19.firebaseapp.com',
+    databaseURL: 'https://iib-swe-ws19.firebaseio.com',
+    projectId: 'iib-swe-ws19',
+    storageBucket: 'iib-swe-ws19.appspot.com',
+    messagingSenderId: '19201567715',
+    appId: '1:19201567715:web:4ae20ff8b578c8186a8ed6',
+}
 
-// firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().enablePersistence({ synchronizeTabs: true })
+
+const firestore = firebase.firestore()
+const storage = firebase.storage()
+const storageRef = storage.ref()
+const auth = firebase.auth()
+const performance = firebase.performance()
+
+const createTimestampFromDate = (date: Date) => firebase.firestore.Timestamp.fromDate(date)
+const createDateFromTimestamp = (timestamp: firebase.firestore.Timestamp) =>
+    new firebase.firestore.Timestamp(timestamp.seconds, timestamp.nanoseconds).toDate()
+const incrementBy = (value: number) => firebase.firestore.FieldValue.increment(value)
+
+export const FirebaseService = {
+    firestore,
+    storage,
+    storageRef,
+    auth,
+    createTimestampFromDate,
+    createDateFromTimestamp,
+    incrementBy,
+    performance,
+}
