d9692de3e860ad83a671bdcb41507649dd9f5a7f
Ilman <i.hamzatov@definemedia.de>
2019-11-21 14:28:16 +0100

    change

--- src/theme.ts
+++ src/theme.ts
@@ -2,3 +2,4 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
 
 const theme = createMuiTheme({ overrides: {} })
 export const responsiveTheme = responsiveFontSizes(theme)
+//jhjjj
