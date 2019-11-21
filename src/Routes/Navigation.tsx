5f847823d24961784968b5ff6271dd462213f3a8
Serhat Sungur <35639693+suse1016@users.noreply.github.com>
2019-11-22 00:14:33 +0100

    Routing (#31)
    * Routing auf verschiedene Ansichten
    
    * Icons ergänzt
    
    * Routen in eigene Komponenten ausgelagert
    
    * TopBar und Navigation in Komponente ausgelagert
    
    * Imports angepasst
    
    * Formattierungsfehler behoben

---
+++ src/Routes/Navigation.tsx
@@ -0,0 +1,37 @@
+import { IconButton } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import EditIcon from '@material-ui/icons/Edit'
+import HomeIcon from '@material-ui/icons/Home'
+import ImageSearchIcon from '@material-ui/icons/ImageSearch'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => (
+    <div>
+        <Link to="/">
+            <IconButton aria-label="create" size="medium">
+                <HomeIcon fontSize="large" />
+            </IconButton>
+        </Link>
+
+        <Link to="/details">
+            <IconButton size="medium">
+                <ImageSearchIcon fontSize="large" />
+            </IconButton>
+        </Link>
+
+        <Link to="/create">
+            <IconButton aria-label="create" size="medium">
+                <AddIcon fontSize="large" />
+            </IconButton>
+        </Link>
+
+        <Link to="/edit">
+            <IconButton aria-label="edit" size="medium">
+                <EditIcon fontSize="large" />
+            </IconButton>
+        </Link>
+    </div>
+)
+
+export default Navigation
