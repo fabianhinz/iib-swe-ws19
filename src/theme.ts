27e2d433224874f155861c02d9a55e484247da72
Oliver <dawson_oliver@yahoo.com>
2019-11-21 15:15:30 +0100

    dark theme added

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
 
-const theme = createMuiTheme({ overrides: {} })
+const theme = createMuiTheme({ palette: { type: 'dark' }, overrides: {} })
 export const responsiveTheme = responsiveFontSizes(theme)
 //jhjjj
 //blabla
