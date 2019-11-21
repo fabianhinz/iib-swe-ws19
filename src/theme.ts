55151f07234d599db0c60e16976029ab3d331a2f
Oliver <dawson_oliver@yahoo.com>
2019-11-21 15:44:31 +0100

    Kommentare gelöscht

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,3 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
 
 const theme = createMuiTheme({ palette: { type: 'dark' }, overrides: {} })
 export const responsiveTheme = responsiveFontSizes(theme)
-//jhjjj
-//blabla
-//afeegege
