73170c25ea1cef72eb4ca8174b4f0ca4b797ff21
siried11 <45973012+siried11@users.noreply.github.com>
2019-11-22 15:56:18 +0100

    Einstiegskomponenten (#33)
    * Navigation und Recipes-Überblick in eigene Dateien
    
    * Einstiegskomponenten erstellt
    
    * finale Einstiegskomponenten

--- src/Components/Create.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export class Create extends React.Component {
-    render() {
-        return <h1> Create </h1>
-    }
-}
--- src/Components/Edit.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export class Edit extends React.Component {
-    render() {
-        return <h1> Edit </h1>
-    }
-}
