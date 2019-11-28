f02fbec639ca69caa523d1df42d1f4cbea27aba5
annikakuntz <46002787+annikakuntz@users.noreply.github.com>
2019-11-28 18:46:26 +0100

    Login  (#34)
    * Login
    
    Login
    
    * small layout changes

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,6 +2,7 @@ import { Container, createStyles, makeStyles } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import algoliasearch from 'algoliasearch/lite'
+import { SnackbarProvider } from 'notistack'
 import React, { FC, useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -9,7 +10,6 @@ import logo from '../icons/logo.svg'
 import Routes from '../Routes/Routes'
 import { responsiveTheme } from '../theme'
 import TopBar from './TopBar'
-
 const searchClient = algoliasearch('OQ8JBQL1SQ', '685ad40e0ddfa41ec3b7bf9605294351')
 export const index = searchClient.initIndex('recipes')
 
@@ -35,13 +35,15 @@ const App: FC = () => {
 
     return (
         <Router>
-            <ThemeProvider theme={responsiveTheme}>
-                <CssBaseline />
-                <Container maxWidth="lg">
-                    <TopBar />
-                    <div className={classes.main}>{Routes}</div>
-                </Container>
-            </ThemeProvider>
+            <SnackbarProvider>
+                <ThemeProvider theme={responsiveTheme}>
+                    <CssBaseline />
+                    <Container maxWidth="lg">
+                        <TopBar />
+                        <div className={classes.main}>{Routes}</div>
+                    </Container>
+                </ThemeProvider>
+            </SnackbarProvider>
         </Router>
     )
 }
