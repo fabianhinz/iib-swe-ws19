30472ba6a47618f1c564484935d73ea4ee21eb0f
Kevin Bosshammer <kevinbosshammer@gmail.com>
2019-12-17 09:21:53 +0100

    Issue 36 & 37 bearbeitet

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent, CardHeader, Grid } from '@material-ui/core'
-import { Box, Button, Container, createStyles } from '@material-ui/core'
+import { Container, createStyles } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { getThemeProps } from '@material-ui/styles'
 import React, { FC, useEffect, useState } from 'react'
 
 import { FirebaseService } from '../../firebase'
@@ -25,37 +24,68 @@ const useStyles = makeStyles(theme =>
     })
 )
 
-interface Recipe {
+interface newRecipe {
     Title: string
-    Categorie: string
+}
+
+interface allRecipe {
+    Title: string
 }
 
 const Home: FC = props => {
-    const [recipes, setRecipes] = useState<Recipe[]>([])
+    const [recipes, setRecipes] = useState<newRecipe[]>([])
+    const [allrecipes, setAllRecipes] = useState<allRecipe[]>([])
     const classes = useStyles()
 
     useEffect(() => {
         FirebaseService.firestore
             .collection('recipes')
-            .orderBy('Creation Date', 'desc')
+            .orderBy('Created', 'desc')
             .limit(6)
             .onSnapshot(querySnaphot => {
-                setRecipes(querySnaphot.docs.flatMap(doc => doc.data()) as Recipe[])
+                setRecipes(querySnaphot.docs.flatMap(doc => doc.data()) as newRecipe[])
+            })
+    }, [])
+
+    useEffect(() => {
+        FirebaseService.firestore
+            .collection('recipes')
+            .orderBy('Title')
+            .onSnapshot(querySnaphot => {
+                setAllRecipes(querySnaphot.docs.flatMap(doc => doc.data()) as allRecipe[])
             })
     }, [])
 
     return (
         <Container className={classes.home}>
+            <h1>Zuletzt Hinzugefügt:</h1>
             <Grid container spacing={2} justify="center">
-                {recipes.map(recipe => (
-                    <Grid key={recipe.Title} item xs={12} md={6} lg={4}>
+                {recipes.map(newrecipe => (
+                    <Grid key={newrecipe.Title} item xs={12} md={6} lg={4}>
                         <Card>
                             <Grid container spacing={2}>
                                 <Grid item xs={4}>
                                     <img src={RecipeImage} alt="RecipeImage" style={imageStyle} />
                                 </Grid>
                                 <Grid item xs={8}>
-                                    <CardHeader title={recipe.Title} />
+                                    <CardHeader title={newrecipe.Title} />
+                                </Grid>
+                            </Grid>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+            <h1>Alle Rezepte:</h1>
+            <Grid container spacing={1} justify="center">
+                {allrecipes.map(allrecipe => (
+                    <Grid key={allrecipe.Title} item xs={12} md={6} lg={4}>
+                        <Card>
+                            <Grid container spacing={2}>
+                                <Grid item xs={4}>
+                                    <img src={RecipeImage} alt="RecipeImage" style={imageStyle} />
+                                </Grid>
+                                <Grid item xs={8}>
+                                    <CardHeader title={allrecipe.Title} />
                                     <CardContent>{props.children}</CardContent>
                                 </Grid>
                             </Grid>
@@ -66,5 +96,4 @@ const Home: FC = props => {
         </Container>
     )
 }
-
 export default Home
