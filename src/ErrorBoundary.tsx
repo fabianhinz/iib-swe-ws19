f9f1148b0a09d35dd5b5d0c2cee3b5f9d1fadcf0
nils musler <muni1019@hs-karlsruhe.de>
2019-12-19 17:08:54 +0100

    final result

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,29 +1,38 @@
 import React from 'react'
 
-class ErrorBoundary extends React.Component {
-    constructor(props: Readonly<{}>) {
+import ErrorImage from './ErrorBoundaryimage.png'
+
+interface ErrorBoundaryState {
+    hasError: boolean
+    error: boolean
+    errorInfo: boolean
+}
+
+class ErrorBoundary extends React.Component<{}, ErrorBoundaryState> {
+    constructor(props: {}) {
         super(props)
-        this.state = { hasError: false }
+        this.state = { hasError: false, error: false, errorInfo: false } //state erweitern
     }
 
-    static getDerivedStateFromError(error: any) {
-        // Update state so the next render will show the fallback UI.
+    static getDerivedStateFromError() {
+        //Lifecycle Method
+        // render a fallback UI
         return { hasError: true }
     }
 
-    componentDidCatch(error: any, errorInfo: any) {
-        // You can also log the error to an error reporting service
+    componentDidCatch() {
+        // log the error Information
 
         this.setState({
-            error: error,
-            errorInfo: errorInfo,
+            error: true,
+            errorInfo: true,
         })
     }
 
     render() {
         if (this.state.hasError) {
             // You can render any custom fallback UI
-            return <h1>Something went wrong.</h1>
+            return <img src={ErrorImage} alt="ErrorImage" />
         }
 
         return this.props.children
